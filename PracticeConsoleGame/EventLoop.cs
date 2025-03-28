a45b7a4d21e4ce22eb2c4f87861977e86579b759
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2025-03-28 16:28:36 +0300

    Event loop wip

--- PracticeConsoleGame/EventLoop.cs
+++ PracticeConsoleGame/EventLoop.cs
@@ -2,13 +2,40 @@ namespace PracticeConsoleGame;
 
 public class EventLoop
 {
-    public event Action? LeftKeyPress;
-    public event Action? RightKeyPress;
-    public event Action? UpKeyPress;
-    public event Action? DownKeyPress;
+    private readonly Dictionary<ConsoleKey, Action> keyLookup;
+
+    public event Action LeftKeyPress = () => { };
+    public event Action RightKeyPress = () => { };
+    public event Action UpKeyPress = () => { };
+    public event Action DownKeyPress = () => { };
 
     public EventLoop()
     {
+        keyLookup = new()
+        {
+            [ConsoleKey.LeftArrow] = LeftKeyPress,
+            [ConsoleKey.A] = LeftKeyPress,
 
+            [ConsoleKey.RightArrow] = RightKeyPress,
+            [ConsoleKey.D] = RightKeyPress,
+
+            [ConsoleKey.UpArrow] = UpKeyPress,
+            [ConsoleKey.W] = UpKeyPress,
+
+            [ConsoleKey.DownArrow] = DownKeyPress,
+            [ConsoleKey.S] = DownKeyPress,
+        };
+    }
+
+    public void Run()
+    {
+        while (true)
+        {
+            var key = Console.ReadKey(true);
+            if (keyLookup.TryGetValue(key.Key, out var action))
+            {
+                action();
+            }
+        }
     }
 }
--- PracticeConsoleGame/Program.cs
+++ PracticeConsoleGame/Program.cs
@@ -1,1 +1,10 @@
-Console.WriteLine("console game");
+using PracticeConsoleGame;
+
+var eventLoop = new EventLoop();
+
+eventLoop.LeftKeyPress += () => Console.WriteLine("left");
+eventLoop.RightKeyPress += () => Console.WriteLine("right");
+eventLoop.UpKeyPress += () => Console.WriteLine("up");
+eventLoop.DownKeyPress += () => Console.WriteLine("down");
+
+eventLoop.Run();
